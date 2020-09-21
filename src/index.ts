8d4832b5c2132505198c1e6bd4ee21fe85148160
Szymon Frąszczak <fraszczak.szymon+github@gmail.com>
2020-09-21 09:22:28 +0200

    Export PostQuecastOptions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Communicator } from './communicator';
 export { Transmitter } from './transmitter';
 export * from './setup/setup';
 export { Action, isActionOfType } from './models/action';
+export { PostQuecastOptions } from './models/options';
